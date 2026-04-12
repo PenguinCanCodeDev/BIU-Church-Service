c1af485177a90437f6b3b83fe745d14e30dd3633
PenguinCanCodeDev <dwightlove375@gmail.com>
2026-04-12 10:10:14 +0200

    Optimized static file routing for Vercel deployment

--- core/settings.py
+++ core/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Use Whitenoise only in production for better local dev performance
 if not DEBUG:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
